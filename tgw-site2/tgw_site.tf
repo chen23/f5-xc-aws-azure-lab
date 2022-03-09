10e33b5dce8161362e0a4ed019362bd6952d334d
Eric Chen <eric.chen@f5.com>
2022-03-08 20:24:34 -0500

    deploy single node site; workaround for lack of 3 az in us-west-1

--- tgw-site2/tgw_site.tf
+++ tgw-site2/tgw_site.tf
@@ -33,31 +33,31 @@ resource "volterra_aws_tgw_site" "aws-region-1" {
       }      
     }
 
-    az_nodes {
-      aws_az_name = var.awsAz2
-      inside_subnet {
-        existing_subnet_id = var.internalSubnets["az2"].id
-      }
-      workload_subnet {
-        existing_subnet_id = var.workloadSubnets["az2"].id      
-      }
-      outside_subnet {
-        existing_subnet_id = var.externalSubnets["az2"].id            
-      }      
-    }
+    # az_nodes {
+    #   aws_az_name = var.awsAz2
+    #   inside_subnet {
+    #     existing_subnet_id = var.internalSubnets["az2"].id
+    #   }
+    #   workload_subnet {
+    #     existing_subnet_id = var.workloadSubnets["az2"].id      
+    #   }
+    #   outside_subnet {
+    #     existing_subnet_id = var.externalSubnets["az2"].id            
+    #   }      
+    # }
 
-    az_nodes {
-      aws_az_name = var.awsAz3
-      inside_subnet {
-        existing_subnet_id = var.internalSubnets["az3"].id
-      }
-      workload_subnet {
-        existing_subnet_id = var.workloadSubnets["az3"].id      
-      }
-      outside_subnet {
-        existing_subnet_id = var.externalSubnets["az3"].id            
-      }      
-    }
+    # az_nodes {
+    #   aws_az_name = var.awsAz3
+    #   inside_subnet {
+    #     existing_subnet_id = var.internalSubnets["az3"].id
+    #   }
+    #   workload_subnet {
+    #     existing_subnet_id = var.workloadSubnets["az3"].id      
+    #   }
+    #   outside_subnet {
+    #     existing_subnet_id = var.externalSubnets["az3"].id            
+    #   }      
+    # }
     aws_cred {
       name = var.volterraCloudCredAWS
       namespace = "system"      
