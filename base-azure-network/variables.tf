2857761560feaea085590799925d367788e2b22f
Marcel Wiget <m.wiget@f5.com>
2022-03-17 07:20:38 +0000

    replace location with azureRegion

--- base-azure-network/variables.tf
+++ base-azure-network/variables.tf
@@ -18,11 +18,6 @@ variable "resourceGroup" {
   default     = "f5demo_rg"
 }
 
-variable "location" {
-  description = "The location/region where the virtual networks are created. Changing this forces a new resource to be created."
-  default     = "eastus"
-}
-
 variable "trusted_ip" {
   description = "IP address of trusted source for mgmt/testing"
   default     = "192.0.2.10/32"
