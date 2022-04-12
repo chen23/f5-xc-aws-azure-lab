b3fca4e250fc4b91ce17bb1de5e090a3d3c12e56
Eric Chen <eric.chen@f5.com>
2022-04-12 15:45:25 -0400

    support multivalue answer for route53

--- tgw-route53-1-to-2/route53.tf
+++ tgw-route53-1-to-2/route53.tf
@@ -11,6 +11,30 @@ resource "aws_route53_health_check" "tgw1" {
   }
 }
 
+resource "aws_route53_health_check" "tgw1b" {
+  ip_address        = var.mesh_public_ips[1]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = format("%s-tgw-1b-healthcheck", var.projectPrefix)
+  }
+}
+
+resource "aws_route53_health_check" "tgw1c" {
+  ip_address        = var.mesh_public_ips[2]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = format("%s-tgw-1c-healthcheck", var.projectPrefix)
+  }
+}
+
 resource "aws_route53_health_check" "tgw2" {
   ip_address        = var.mesh_public_ips2[0]
   port              = 80
@@ -23,40 +47,240 @@ resource "aws_route53_health_check" "tgw2" {
   }
 }
 
+resource "aws_route53_health_check" "tgw2b" {
+  ip_address        = var.mesh_public_ips2[1]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
 
- resource "aws_route53_record" "globalworkloadtgw1" {
-   zone_id = var.route54zoneid
-   name    = "*.global.example.internal"
-   type    = "CNAME"
-   ttl     = "300"
+  tags = {
+    Name = format("%s-tgw-2b-healthcheck", var.projectPrefix)
+  }
+}
 
-   health_check_id = aws_route53_health_check.tgw1.id
+resource "aws_route53_health_check" "tgw2c" {
+  ip_address        = var.mesh_public_ips2[2]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
 
-   failover_routing_policy {
-   type = "PRIMARY"
-   }
+  tags = {
+    Name = format("%s-tgw-2c-healthcheck", var.projectPrefix)
+  }
+}
 
-   set_identifier = format("%s-tgw-1", var.projectPrefix)
 
-   records = [var.tgw1nlbdns]
- }
+resource "aws_route53_health_check" "azure1" {
+  ip_address        = var.mesh_public_ips3[0]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
 
- resource "aws_route53_record" "globalworkloadtgw1backup" {
-   zone_id = var.route54zoneid
-   name    = "*.global.example.internal"
-   type    = "CNAME"
-   ttl     = "300"
+  tags = {
+    Name = format("%s-azure-1-healthcheck", var.projectPrefix)
+  }
+}
 
-   health_check_id = aws_route53_health_check.tgw2.id
+resource "aws_route53_health_check" "azure1b" {
+  ip_address        = var.mesh_public_ips3[1]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
 
-   failover_routing_policy {
-   type = "SECONDARY"
-   }
+  tags = {
+    Name = format("%s-azure-1b-healthcheck", var.projectPrefix)
+  }
+}
 
-   set_identifier = format("%s-tgw-2", var.projectPrefix)
+resource "aws_route53_health_check" "azure1c" {
+  ip_address        = var.mesh_public_ips3[2]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
 
-   records = [aws_lb.ext-nlb-tgw2.dns_name]
- }
+  tags = {
+    Name = format("%s-azure-1c-healthcheck", var.projectPrefix)
+  }
+}
+
+resource "aws_route53_health_check" "azure2" {
+  ip_address        = var.mesh_public_ips4[0]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = format("%s-azure-2-healthcheck", var.projectPrefix)
+  }
+}
+
+resource "aws_route53_health_check" "azure2b" {
+  ip_address        = var.mesh_public_ips4[1]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = format("%s-azure-2b-healthcheck", var.projectPrefix)
+  }
+}
+
+resource "aws_route53_health_check" "azure2c" {
+  ip_address        = var.mesh_public_ips4[2]
+  port              = 80
+  type              = "TCP"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = format("%s-azure-2c-healthcheck", var.projectPrefix)
+  }
+}
+
+
+resource "aws_route53_record" "globalworkloadtgw1" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw1.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-1", var.projectPrefix)
+
+  records = [var.mesh_private_ips[0]]
+}
+
+resource "aws_route53_record" "globalworkloadtgw1b" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw1b.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-1b", var.projectPrefix)
+
+  records = [var.mesh_private_ips[1]]
+}
+
+resource "aws_route53_record" "globalworkloadtgw1c" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw1c.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-1c", var.projectPrefix)
+
+  records = [var.mesh_private_ips[2]]
+}
+
+resource "aws_route53_record" "globalworkloadtgw2" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw2.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-2", var.projectPrefix)
+
+  records = [var.mesh_private_ips2[0]]
+}
+
+resource "aws_route53_record" "globalworkloadtgw2b" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw2b.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-2b", var.projectPrefix)
+
+  records = [var.mesh_private_ips2[1]]
+}
+
+resource "aws_route53_record" "globalworkloadtgw2c" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.tgw2c.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-tgw-2c", var.projectPrefix)
+
+  records = [var.mesh_private_ips2[2]]
+}
+
+
+resource "aws_route53_record" "globalworkloadazure1" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.azure1.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-azure-1", var.projectPrefix)
+
+  records = [var.mesh_private_ips3[0]]
+}
+
+resource "aws_route53_record" "globalworkloadazure1b" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.azure1b.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-azure-1b", var.projectPrefix)
+
+  records = [var.mesh_private_ips3[1]]
+}
+
+resource "aws_route53_record" "globalworkloadazure1c" {
+  zone_id = var.route54zoneid
+  name    = "*.global.example.internal"
+  type    = "A"
+  ttl     = "300"
+
+  health_check_id = aws_route53_health_check.azure1c.id
+
+  multivalue_answer_routing_policy = true
+
+  set_identifier = format("%s-azure-1c", var.projectPrefix)
+
+  records = [var.mesh_private_ips3[2]]
+}
+
 
 
 resource "aws_route53_zone" "private" {
@@ -67,6 +291,8 @@ resource "aws_route53_zone" "private" {
     vpc_id = var.vpcId2
   }
 }
+
+
 
 
 # resource "aws_route53_record" "tgw2workload" {
