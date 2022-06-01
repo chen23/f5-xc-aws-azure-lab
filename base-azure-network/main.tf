ed7617d5989fd1d3e42482f4f2d81279604d6c21
thomsonjames <james@f5.com>
2022-06-01 11:24:45 -0400

    changed nsg to Tcp instead of TCP

--- base-azure-network/main.tf
+++ base-azure-network/main.tf
@@ -111,7 +111,7 @@ resource "azurerm_network_security_rule" "allow_http" {
   priority                    = 210
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "TCP"
+  protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "80"
   source_address_prefix       = "*"
